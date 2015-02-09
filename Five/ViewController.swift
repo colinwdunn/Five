20365e5cafed52c163b136ee0401a5b73e0e9a38
Colin Dunn <colin@colindunn.com>
2015-02-08 20:23:58 -0800

    Moved type/name logic to view controller

--- Five/ViewController.swift
+++ Five/ViewController.swift
@@ -105,18 +105,22 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         var startTime = NSDate()
         
+        let typeZeroNames = [exerciseName.Squat.rawValue, exerciseName.BenchPress.rawValue, exerciseName.Row.rawValue]
+        let typeOneNames = [exerciseName.Squat.rawValue, exerciseName.OverheadPress.rawValue, exerciseName.Deadlift.rawValue]
+        
         for i in 1...3 {
             let record = CKRecord(recordType: "Exercise")
             record.setObject(startTime, forKey: "startTime")
-            record.setObject(0, forKey: "Name")
             record.setObject(45 * i, forKey: "Weight")
             record.setObject(0, forKey: "Reps")
             
             if lastTypeIsZero {
                 record.setObject(1, forKey: "Type")
+                record.setObject(typeOneNames[i - 1], forKey: "Name")
                 println("Created type 1")
             } else {
                 record.setObject(0, forKey: "Type")
+                record.setObject(typeZeroNames[i - 1], forKey: "Name")
                 println("Creatd type 0")
             }
             
@@ -135,9 +139,11 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             }
         }
         
-        let detailViewController = DetailViewController()
-        detailViewController.exercisesForDay = days[0]
-        self.navigationController?.pushViewController(detailViewController, animated: true)
+        if !days.isEmpty {
+            let detailViewController = DetailViewController()
+            detailViewController.exercisesForDay = days[0]
+            self.navigationController?.pushViewController(detailViewController, animated: true)
+        }
     }
     
     func removeItem(item: CKRecord) {
@@ -202,7 +208,7 @@ extension ViewController: UITableViewDataSource {
         let dateFormatter = NSDateFormatter()
         dateFormatter.dateFormat = "EEEE, MMM d"
         let dateString = dateFormatter.stringFromDate(date)
-        cell.date.text = dateString
+        cell.date.text = "\(dateString) (\(data.count))"
         
         cell.exerciseOne.text = exerciseName(rawValue: data[0].objectForKey("Name") as Int)?.description()
         cell.exerciseTwo.text = exerciseName(rawValue: data[1].objectForKey("Name") as Int)?.description()
